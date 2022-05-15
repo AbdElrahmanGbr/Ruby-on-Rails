98f8e6b9668db9c0d441d057deb4531f5fccf0fb
AbdElrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-05-15 15:55:26 +0200

    starting with devise

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,16 @@ class CommentsController < ApplicationController
       redirect_to article_path(@article)
     end
   
-    private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body, :status)
+    def destroy
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
+      @comment.destroy
+      redirect_to article_path(@article), status: 303
     end
+  
+    private
+      def comment_params
+        params.require(:comment).permit(:commenter, :body, :status)
+      end
   end
+  
